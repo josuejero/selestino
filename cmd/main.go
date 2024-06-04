861d8d9338cc0cdd5ab94c618dd7a40ec208025d
josue <josuejero@hotmail.com>
2024-06-04 18:30:41 -0400

    Set up PostgreSQL connection

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,13 @@ import (
 	"net/http"
 
 	"github.com/josuejero/selestino/api"
+	"github.com/josuejero/selestino/pkg/config"
 )
 
 func main() {
+	db := config.Connect()
+	defer db.Close()
+
 	router := api.InitializeRouter()
 
 	log.Println("Starting server on :8080")
