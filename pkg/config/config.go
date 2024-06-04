4604125624c98de2f1b51d19bd8f0d3f4f61e957
josue <josuejero@hotmail.com>
2024-06-04 18:30:41 -0400

    Set up PostgreSQL connection

---
+++ pkg/config/config.go
@@ -0,0 +1,36 @@
+// pkg/config/config.go
+
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "selestino_user"
+	password = "your_password"
+	dbname   = "selestino"
+)
+
+func Connect() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+	return db
+}
