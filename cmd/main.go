5be16ab4000c6ed4c468a07b04e6e96aed691cba
josue <josuejero@hotmail.com>
2024-06-05 21:49:18 -0400

    Add continuous deployment and logging enhancements

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,25 @@
-// cmd/main.go
+// main.go
 
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/josuejero/selestino/api"
 	"github.com/josuejero/selestino/pkg/config"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	db := config.Connect()
 	defer db.Close()
 
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
 	router := api.InitializeRouter(db)
 
-	log.Println("Starting server on :8080")
+	logrus.Info("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatalf("could not start server: %v\n", err)
+		logrus.Fatalf("could not start server: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
